6a2884102a85b14f66b0bf99df0337b96cfe0057
sklass <47379975+sklass@users.noreply.github.com>
2019-02-22 19:22:23 +0100

    Auto um neue methoden erweitert

--- Auto/src/Auto.java
+++ Auto/src/Auto.java
@@ -16,16 +16,61 @@ public class Auto {
         this.Seriennummer = Seriennummer;
         this.TankVolumen = TankVolumen;
         this.Verbrauch = Verbrauch;
+        this.TankStand = 0;
     }
 
     void druckeEigenschaften(){
-        System.out.println("\nDas Fahrzeug : " + Marke + " " + Modell + " hat folgende Eigenschaften: \nFarbe: " + Farbe + "\nSeriennummer: " + Seriennummer + "\nTankVolumen: " + TankVolumen + "\nVerbrauch je 100Km: " + Verbrauch + "L");
+        System.out.println("\nDas Fahrzeug " + Marke + " " + Modell + " hat folgende Eigenschaften: \nFarbe: " + Farbe + "\nSeriennummer: " + Seriennummer + "\nTankVolumen: " + TankVolumen + "\nVerbrauch je 100Km: " + Verbrauch + "L");
     }
 
-    //gebeAktuelleReichweite
-    //tankstand / verbrauch * 100
+     double maximalReichweite(){
+        double reichweite = TankVolumen / Verbrauch * 100;
+        return reichweite;
+    }
+
+    double holeAktuelleReichweite(){
+        return TankStand / Verbrauch * 100;
+    }
 
     //fahre(Anzahl km)
+    boolean fahre(double kilometer){
+        double FahrtVerbrauch = kilometer *  Verbrauch / 100;
+        double maximaleStrecke = TankStand / Verbrauch;
+        if(FahrtVerbrauch > TankStand){
+            System.out.println("Tankfüllung reicht nicht um so weit zu Fahren");
+        }else{
+            TankStand -= FahrtVerbrauch;
+            System.out.printf("Die Fahrt hat %.2f L Benzin verbraucht\n", FahrtVerbrauch);
+            druckeTankstand();
+        }
+        return true;
+    }
 
-    //tanke(Anzahl Liter)
+    double holeTankstand(){
+        return TankStand;
+    }
+
+    void tanke(double Liter){
+        double PlatzImTank = TankVolumen-TankStand;
+        if(Liter < 0 ){
+            System.out.println("Keine negativen Werte möglich");
+            Liter = 0;
+        }
+        if(Liter > PlatzImTank ){
+            System.out.println("Maximales Tankvolumen überschritten!\nSetze Anzahl Liter auf " + PlatzImTank);
+            Liter = PlatzImTank;
+        }
+        System.out.println(Liter + " L getankt");
+        TankStand += Liter;
+        druckeTankstand();
+    }
+
+    void druckeReichweite(){
+        System.out.printf("Die Reichweite bei aktueller Tankfüllung beträgt %.2f Km\n", holeAktuelleReichweite());
+    }
+
+    void druckeTankstand(){
+        System.out.printf("Der aktuelle Tankstand beträgt %.1f L\n", holeTankstand());
+    }
+
 }
--- Auto/src/Main.java
+++ Auto/src/Main.java
@@ -5,10 +5,9 @@ public class Main {
 
     public static void main(String[] args) {
         erzeugeAutos();
-        AudiA4.druckeEigenschaften();
-        AudiA6.druckeEigenschaften();
-        BMW3er.druckeEigenschaften();
-
+        AudiA4();
+        AudiA6();
+        BMW3er();
     }
 
     static void erzeugeAutos(){
@@ -16,8 +15,33 @@ public class Main {
         AudiA6 = new Auto();
         BMW3er = new Auto();
 
-        AudiA4.setzeEigenschaften("Audi","A4", "Rot", "12345-12345-12345", 90, 7.5);
-        AudiA6.setzeEigenschaften("Audi","A6", "Blau", "67890-67890-67890", 120, 8.5);
-        BMW3er.setzeEigenschaften("BMW","3er", "Silber", "101112-101112-101112", 120, 8.5);
+        AudiA4.setzeEigenschaften("Audi","A4", "Rot", "12345-12345-12345", 65, 8.5);
+        AudiA6.setzeEigenschaften("Audi","A6", "Blau", "67890-67890-67890", 70, 10.5);
+        BMW3er.setzeEigenschaften("BMW","3er", "Silber", "101112-101112-101112", 55, 9.0);
+    }
+
+    static void AudiA4(){
+        AudiA4.druckeEigenschaften();
+        System.out.printf("Die maximale Reichweite des Wagens beträgt %.1f Km\n", AudiA4.maximalReichweite());
+        AudiA4.druckeTankstand();
+        AudiA4.tanke(25);
+        AudiA4.druckeReichweite();
+    }
+
+    static void AudiA6(){
+        AudiA6.druckeEigenschaften();
+        System.out.printf("Die maximale Reichweite des Wagens beträgt %.1f Km\n", AudiA6.maximalReichweite());
+        AudiA6.druckeTankstand();
+        AudiA6.tanke(70);
+        AudiA6.druckeReichweite();
+        AudiA6.fahre(200);
+
+    }
+
+    static void BMW3er(){
+        BMW3er.druckeEigenschaften();
+        System.out.printf("Die maximale Reichweite des Wagens beträgt %.1f Km\n", BMW3er.maximalReichweite());
+        BMW3er.druckeReichweite();
+        AudiA6.fahre(273);
     }
 }
